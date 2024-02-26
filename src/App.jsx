62cb5fb864457a5466a4c2518ec62b24b737a7cb
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-02-26 15:06:37 +0100

    components+redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ContactList } from "./components/ContactList/ContactList";
+import { SearchBar } from "./components/SearchBar/SearchBar";
+import { ConctactForm } from "./components/ContactForm/ContactForm";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Loader } from "./components/Loader/Loader";
+import { getLoading } from "./redux/selector";
+import { fetchContacts } from "./redux/Numbers/operationNumber";
 
+export const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+  const isLoading = useSelector(getLoading);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Phonebook</h1>
+      <ConctactForm />
+      <SearchBar />
+      <ContactList />
+      {isLoading && <Loader />}
     </>
-  )
-}
-
-export default App
+  );
+};
