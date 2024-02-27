b5070949f1648d455e1edd340b9ff03de6768722
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-02-27 21:31:58 +0100

    number

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,10 @@
-import { FaPerson } from "react-icons/fa6";
-import { FaPhoneAlt } from "react-icons/fa";
+import { FaPerson } from 'react-icons/fa6';
+import { FaPhoneAlt } from 'react-icons/fa';
 
-import css from "./Contact.module.css";
+import css from './Contact.module.css';
 
-import { useDispatch } from "react-redux";
-import { fetchDeleteContact } from "../../redux/Numbers/operationNumber";
+import { useDispatch } from 'react-redux';
+import { fetchDeleteContact } from '../../redux/Numbers/operationNumber';
 
 export const Contact = ({ dataContact: { id, name, number } }) => {
   const dispatch = useDispatch();
